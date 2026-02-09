e69a7638e08e75ac8f459fd050f45c0b13183ad8
CDatarET <datar.chaitanya@gmail.com>
2026-02-09 11:44:44 +0530

    fixed display

--- 3.cpp
+++ 3.cpp
@@ -33,13 +33,34 @@ class Graph{
         }
 
         void display(){
+            cout << "  ";
+            for(int i = 0; i < n; i++){
+                cout << i << " ";
+            }
+            cout << endl << "  ";
+
+            for(int i = 0; i < n - 1; i++){
+                cout << "--";
+            }
+            cout << "-" << endl;
+
             for(int i = 0; i < n; i++){
                 GraphNode *current = heads[i];
-                while(current != NULL){
-                    cout << current->vertex << " ";
-                    current = current->next;
+                cout << i << "|";
+                for(int j = 0; j < n; j++){
+                    if(current == NULL){
+                        cout << "0 ";
+                    }
+                    else{
+                        if(current->vertex == j){
+                            cout << "1 ";
+                            current = current->next;
+                        }
+                        else{
+                            cout << "0 ";
+                        }
+                    }
                 }
-
                 cout << endl;
             }
         }
