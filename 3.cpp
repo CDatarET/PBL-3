f7d95d275876c3c458bea2dc2d4767bb90501e55
CDatarET <datar.chaitanya@gmail.com>
2026-02-27 23:09:39 +0530

    small display fix

--- 3.cpp
+++ 3.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class GraphNode{
     int vertex;
+    string name;
     GraphNode *next;
     friend class Graph;
 };
@@ -17,22 +18,32 @@ class Graph{
             cout << "Enter # of Vertices: ";
             cin >> n;
             for(int i = 0; i < n; i++){
+                cout << "Enter name: ";
+                string name;
+                cin >> name;
+
                 cout << "How many adjacent vertices?: ";
                 int edges;
                 cin >> edges;
 
-                cout << "Enter connections: ";
+                cout << "Enter connections for " << name << ", ID = " << i << ": ";
                 heads[i] = new GraphNode;
-                GraphNode *current = heads[i];
+                heads[i]->name = name;
+                heads[i]->vertex = i;
+                heads[i]->next = new GraphNode;
+
+                GraphNode *current = heads[i]->next;
                 for(int j = 0; j < edges; j++){
                     cin >> current->vertex;
                     current->next = new GraphNode;
                     current = current->next;
                 }
+
+                current->next = NULL;
             }
         }
 
-        void display(){
+        void displayMatrix(){
             cout << "  ";
             for(int i = 0; i < n; i++){
                 cout << i << " ";
@@ -61,13 +72,52 @@ class Graph{
                         }
                     }
                 }
-                cout << endl;
+                cout << endl
+            }
+
+            cout << endl;
+        }
+
+        void displayList(){
+            for(int i = 0; i < n; i++){
+                GraphNode *current = heads[i];
+                while(current != NULL){
+                    cout << heads[current->vertex]->name;
+                    if(current->next != NULL){
+                        cout << "->";
+                    }
+                    else{
+                        cout << endl;
+                    }
+
+                    current = current->next;
+                }
             }
         }
+
+        /*
+        void DFS(int v, int visited[]){
+            cout << v << ",";
+            visited[v] = 1;
+            
+        }
+
+        void DFS(){
+            int v;
+            int visited[n];
+            for(int i = 0 i < n; i++){
+                visited[i] = 0;
+            }
+
+
+        }
+        
+        */
 };
 
 int main(){
     Graph g;
-    g.display();
+    g.displayMatrix();
+    g.displayList();
     return(0);
 }
